bb041bee796a5885376896e48c631a8428079d7f
DiogodosSantosPastor <diogo_pastor@estudante.sesisenai.org.br>
2024-08-19 23:27:46 -0300

    3 commit

--- app/(tabs)/Lista Tarefa/index.jsx
+++ app/(tabs)/Lista Tarefa/index.jsx
@@ -4,13 +4,13 @@ import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 const Item = ({ title, feito }) => (
   <View style={[styles.item, feito ? styles.doneItem : styles.notDoneItem]}>
-      <Text style={feito ? styles.doneTitle : styles.title}>{title}</Text>
+    <Text style={feito ? styles.doneTitle : styles.title}>{title}</Text>
   </View>
 );
 
 export default function listatarefa(){
       const TAREFAS = [
-      {id: '1', title: 'A', feito: false},
+      {id: '1', title: 'A', feito: true},
       {id: '2', title: 'B', feito: false},
       {id: '3', title: 'C', feito: false},
       {id: '4', title: 'D', feito: false},
@@ -18,15 +18,15 @@ export default function listatarefa(){
     ];
 
 return(
-        <View style={styles.container}>
-          <Text style={styles.Text}>Lista de tarefas</Text>
-            <FlatList
+      <View style={styles.container}>
+        <Text style={styles.headerText}>Lista de Tarefas</Text>
+        <FlatList
           data={TAREFAS}
-          renderItem={({item}) => <Item title={item.title} />}
+          renderItem={({item}) => <Item title={item.title} feito={item.feito} />}
           keyExtractor={item => item.id}
-      />
-        </View>
-    );
+        />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -34,19 +34,30 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
   },
-  Text:{
+  headerText:{
       fontSize: 49,
       fontWeight: 'bold',
       marginBottom: 40,
   },
   item: {
-      backgroundColor: '#F91514',
+      backgroundColor: '#ff0a0a',
       padding: 14,
       marginVertical: 8,
       marginHorizontal: 16,
     },
     title: {
       fontSize: 18,
-      
+      color: '#000000',
+    },
+    doneTitle: {
+      fontSize: 18,
+      color: '#000000',
+      textDecorationLine: 'line-through',
+    },
+    doneItem: {
+      backgroundColor: '#ff0a0a',
+    },
+    notDoneItem: {
+      backgroundColor: '#fc4141',
     },   
 });
